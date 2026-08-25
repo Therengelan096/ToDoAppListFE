c83909082220160be06519488e5447f61e92ce90
Therengelan096 <anderson.alvarez.rengel.096@gmail.com>
2026-08-25 19:07:34 -0400

    fix: incluye api.js

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+// src/services/api.js
+
+export const API_URL = "http://127.0.0.1:8000/api";
+
+export const DEFAULT_HEADERS = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+
+export const handleResponse = async (response) => {
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({}));
+    throw new Error(
+      errorData.message || `HTTP error! status: ${response.status}`,
+    );
+  }
+  return await response.json();
+};
