0dab0ac941f55f4444a8e7e22cdfa14565cbb3ee
Therengelan096 <anderson.alvarez.rengel.096@gmail.com>
2026-08-26 16:09:26 -0400

    TDO-5: Implementar función getAll() en tasks (#4)

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+export const API_URL = "http://127.0.0.1:8000/api";
+
+export const DEFAULT_HEADERS = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+
+export const handleResponse = async (response) => {
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({}));
+    throw new Error(
+      errorData.message || `HTTP error! status: ${response.status}`,
+    );
+  }
+  return await response.json();
+};
---
+++ src/services/tarea.service.js
@@ -0,0 +1,21 @@
+import { API_URL, DEFAULT_HEADERS, handleResponse } from "./api";
+
+export const getAll = async () => {
+  try {
+    const response = await fetch(`${API_URL}/tasks`, {
+      method: "GET",
+      headers: DEFAULT_HEADERS,
+    });
+
+    const data = await handleResponse(response);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const create = async (taskData) => {};
+
+export const update = async (id, taskData) => {};
+
+export const remove = async (id) => {};
